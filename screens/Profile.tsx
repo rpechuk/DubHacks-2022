742c8838de2f19e2ebc5cbd8024d5912d4498694
ronpechuk <rpechuk@gmail.com>
2022-10-22 19:01:34 -0700

    finished profile switch

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component, useEffect, useState } from "react";
 import {
   ScrollView,
   View,
@@ -6,11 +6,31 @@ import {
   ImageBackground,
   TouchableOpacity,
 } from "react-native";
-import { Icon, ProfileItem } from "../components";
+import { ProfileItem } from "../components";
 import DEMO from "../assets/data/demo";
 import styles, { WHITE } from "../assets/styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { OmitProps } from "antd/lib/transfer/ListBody";
+
+const getData = async (): Promise<boolean> => {
+  try {
+    const value = await AsyncStorage.getItem('signed_up');
+    return value === null
+  } catch(e) {
+    console.error(e);
+    return false
+  }
+}
 
 const Profile = () => {
+  const [needSignUp, setNeedSignUp] = useState(false)
+
+  useEffect(() => {
+    getData().then((val) => {
+      setNeedSignUp(needSignUp => val);
+    });
+  });
+
   const {
     image,
     name,
@@ -22,27 +42,51 @@ const Profile = () => {
     hikeType,
   } = DEMO[7];
 
-  return (
-    <ImageBackground
-      source={require("../assets/images/bg.png")}
-      style={styles.bg}
-    >
-      <ScrollView style={styles.containerProfile}>
-        <ImageBackground source={image} style={styles.photo}>
-        </ImageBackground>
-
-        <ProfileItem
-          name={name}
-          age={age}
-          gender={gender}
-          location={location}
-          level={level}
-          drive={drive}
-          hikeType={hikeType}
-        />
-      </ScrollView>
-    </ImageBackground>
-  );
+  if (needSignUp) {
+    return (
+      <ImageBackground
+        source={require("../assets/images/bg.png")}
+        style={styles.bg}
+      >
+        <ScrollView style={styles.containerProfile}>
+          <ImageBackground source={image} style={styles.photo}>
+          </ImageBackground>
+  
+          <ProfileItem
+            name={name}
+            age={age}
+            gender={gender}
+            location={location}
+            level={level}
+            drive={drive}
+            hikeType={hikeType}
+          />
+        </ScrollView>
+      </ImageBackground>
+    );
+  } else {
+    return (
+      <ImageBackground
+        source={require("../assets/images/bg.png")}
+        style={styles.bg}
+      >
+        <ScrollView style={styles.containerProfile}>
+          <ImageBackground source={image} style={styles.photo}>
+          </ImageBackground>
+  
+          <ProfileItem
+            name={name}
+            age={age}
+            gender={gender}
+            location={location}
+            level={level}
+            drive={drive}
+            hikeType={hikeType}
+          />
+        </ScrollView>
+      </ImageBackground>
+    );
+  }
 };
 
 export default Profile;
